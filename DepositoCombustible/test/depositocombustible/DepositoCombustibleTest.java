ff049a3843e549faa75e031c618be9a5fe6110d7
Jaime Sansano Garcia <jasano2@hotmail.com>
2020-01-19 07:40:10 +0100

    metodos testEstaVacio(), testEstaLleno(), testMitadMax() y añadir valor delta en assertEquals(double, double, delta)

--- DepositoCombustible/test/depositocombustible/DepositoCombustibleTest.java
+++ DepositoCombustible/test/depositocombustible/DepositoCombustibleTest.java
@@ -9,27 +9,41 @@ public class DepositoCombustibleTest {
 	@Test
 	public void testGetDepositoNivel() {
 		DepositoCombustible deposito = new DepositoCombustible(50.0,0.0);
-		Double niv = 0.0;
-		Double nivel = deposito.getDepositoNivel();
-		assertEquals(niv, nivel);
+		double niv = 0.0;
+		double nivel = deposito.getDepositoNivel();
+		assertEquals(niv, nivel, 1);
 	}
 
 	@Test
 	public void testGetDepositoMax() {
 		DepositoCombustible deposito = new DepositoCombustible(50.0,0.0);
-		Double max = 50.0;
-		Double maximo = deposito.getDepositoMax();
-		assertEquals(max, maximo);
+		double max = 50.0;
+		double maximo = deposito.getDepositoMax();
+		assertEquals(max, maximo, 1);
 	}
 
 	@Test
 	public void testEstaVacio() {
-		fail("Not yet implemented");
+		DepositoCombustible deposito = new DepositoCombustible(50.0,0.0);
+		boolean vac = true;
+		boolean vacio = deposito.estaVacio();
+		assertTrue(vac == vacio);
 	}
 
 	@Test
 	public void testEstaLleno() {
-		fail("Not yet implemented");
+		DepositoCombustible deposito = new DepositoCombustible(50.0,50.0);
+		boolean lle = true;
+		boolean lleno = deposito.estaLleno();
+		assertTrue(lle == lleno);
+	}
+
+	@Test
+	public void testMitadMax() {
+		DepositoCombustible deposito = new DepositoCombustible(50.0,25.0);
+		double mit = 25.0;
+		double mitad = deposito.getDepositoMax() / 2;
+		assertEquals(mit, mitad, 1);
 	}
 
 	@Test
