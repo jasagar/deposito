347d84fe28ee17d827e338ba3b0e12bce405c181
Jaime Sansano Garcia <jasano2@hotmail.com>
2020-01-19 20:23:57 +0100

    creacion clase DepositoSuitePruebas.java

---
+++ DepositoCombustible/test/depositocombustible/DepositoSuitePruebas.java
@@ -0,0 +1,12 @@
+package depositocombustible;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ DepositoCombustibleTest.class, DepositoConsumirTestParametrizado.class,
+		DepositoFillTestParametrizado.class })
+public class DepositoSuitePruebas {
+
+}
