fd49e954a3052d198acdeed7dbba1f30284a3ae8
Jaime Sansano Garcia <jasano2@hotmail.com>
2020-01-19 09:59:23 +0100

    creacion clase DepositoCombustibleTestParametrizado.java

---
+++ DepositoCombustible/test/depositocombustible/DepositoCombustibleTestParametrizado.java
@@ -0,0 +1,24 @@
+package depositocombustible;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class DepositoCombustibleTestParametrizado {
+	//private
+
+	@Test
+	public void testFill() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConsumir() {
+		fail("Not yet implemented");
+	}
+
+}
